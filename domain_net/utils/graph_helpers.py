5719698d8975a4234cff1313b38014524ff58e92
leventidis <leventidis.a@husky.neu.edu>
2022-09-20 11:28:43 -0400

    Updated notebook

--- domain_net/utils/graph_helpers.py
+++ domain_net/utils/graph_helpers.py
@@ -9,3 +9,14 @@ def get_attribute_of_instance(G, instance_node):
         if G.nodes[neighbor]['type'] == 'attr':
             attribute_nodes.append(neighbor)
     return attribute_nodes
+
+def get_cell_node_column_names(G, cell_node):
+    '''
+    Given a graph `G` and a `cell_node` return the unique column names of all its connected attribute nodes
+    Note: This function only works for graphs `G` where attribute nodes have a 'column_name' type (i.e. synthetic benchmark graphs) 
+    '''
+    attribute_nodes = get_attribute_of_instance(G, cell_node)
+    column_names = []
+    for attr in attribute_nodes:
+        column_names.append(G.nodes[attr]['column_name'])
+    return list(set(column_names))
